ec0df7b75150ce53747722a91065a6550452c512
shahna shah <shahanashah9630@gmail.com>
2025-09-04 23:59:49 +0530

    chore(mobile): add Capacitor core, android/ios, Filesystem & Share; base config

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -3,7 +3,11 @@ import type { CapacitorConfig } from '@capacitor/cli';
 const config: CapacitorConfig = {
   appId: 'com.wealthelite.dashboard',
   appName: 'WealthElite',
-  webDir: 'out'
+  webDir: 'out',
+  bundledWebRuntime: false,
+  server: {
+    androidScheme: 'https'
+  }
 };
 
 export default config;
